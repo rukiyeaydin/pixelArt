9a12b6288ee41e6a9a222cabffdd5cd1ec1c79ec
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-07-22 20:10:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const inputbg = document.querySelector(".inputbg");
+const container = document.querySelector(".container");
+const brush = document.querySelector(".brush");
+const kareler = document.querySelector(".kareler");
+
+inputbg.addEventListener("change", () => {
+    newValue = inputbg.value;
+    document.querySelector(".container").style.backgroundColor = newValue;
+}) 
+
+let tikla = false;
+
+for(let i = 0; i < 20; i++){
+    for(let j = 0; j < 20; j++)
+    {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        container.appendChild(pixel);
+
+        pixel.addEventListener("mouseover", function(){
+            if(!tikla) return
+            pixel.style.backgroundColor = brush.value;
+        })
+
+        pixel.addEventListener("mousedown",function(){
+            pixel.style.backgroundColor = brush.value;
+        })
+    }
+}
+
+kareler.addEventListener("change", () => {
+    newKareValue = kareler.value;
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+      pixel.style.backgroundColor = newKareValue;
+    });
+});
+
+window.addEventListener("mousedown", function(){
+    tikla = true;
+})
+
+window.addEventListener("mouseup",function(){
+    tikla = false;
+})
+
+
